870d264812394038035333748461403002511ae0
sainath <sainath90@gmail.com>
2017-10-31 22:50:48 -0400

    My project setup

---
+++ ecommercebackend/src/main/java/com/sai/ecommercebackend/App.java
@@ -0,0 +1,13 @@
+package com.sai.ecommercebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
